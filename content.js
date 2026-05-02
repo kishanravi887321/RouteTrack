cf516b90824d63e14de76d3f0a0c030e9971ab07
Ravi Kishan <kishanravi887321@gmail.com>
2026-05-03 01:14:59 +0530

    Remove unused CSS and JavaScript files, update manifest to load overlay module

--- content.js
+++
@@ -1,5 +0,0 @@
-const overlayModuleUrl = chrome.runtime.getURL("src/overlay/index.js");
-
-import(overlayModuleUrl).catch((error) => {
-  console.error("RouteTracker overlay failed to load", error);
-});
